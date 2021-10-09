8e3e10019effd99e0a18ae433e616a26884ff59a
Kennion Gubler <gublerkennion@gmail.com>
2021-10-09 17:48:32 -0600

    final

---
+++ Lesson04/script.js
@@ -0,0 +1,13 @@
+
+function dateFunction(){
+  var time = document.lastModified;
+  document.write(time);
+}
+    function myFunction() {
+      var x = document.getElementById("navbar");
+      if (x.style.display === "block"){
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+  }
