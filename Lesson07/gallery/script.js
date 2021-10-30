b56be94868e2f1d4413ffcac79888f93b0664358
Kennion Gubler <gublerkennion@gmail.com>
2021-10-30 12:27:43 -0600

    initial

---
+++ Lesson07/gallery/script.js
@@ -0,0 +1,60 @@
+const images = document.querySelectorAll('[data-src]');
+
+const preloadImage = (img) => {
+    const src = img.getAttribute('data-src');
+    if (!src) {
+        return;
+    }
+    img.src = src;
+}
+
+const imgOptions = {
+    threshod: 0,
+    rootMargin: '0px 0px -200px 0px',
+};
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.style.animation = 'bounce ease-in 500ms'
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+images.forEach((image) => {
+    imgObserver.observe(image)
+})
+
+const images = document.querySelectorAll('[data-src]');
+
+const preloadImage = (img) => {
+    const src = img.getAttribute('data-src');
+    if (!src) {
+        return;
+    }
+    img.src = src;
+}
+
+const imgOptions = {
+    threshod: 0,
+    rootMargin: '0px 0px 2200px 0px',
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.style.animation = 'bounce ease-in 500ms'
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+images.forEach((image) => {
+    imgObserver.observe(image)
+})
